c5927c6b0d8acff191a3a5d5e4eea89ef84fe2d4
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-03-18 16:48:13 +0100

    bug fix

--- src/main/java/Network/SocketBroadcaster.java
+++ src/main/java/Network/SocketBroadcaster.java
@@ -13,7 +13,6 @@ public class SocketBroadcaster {
 
     public void send(String message) throws UnknownHostException, SocketException, IOException {
         DatagramSocket socket = new DatagramSocket();//this.port, InetAddress.getByName("255.255.255.255"));
-        socket.setBroadcast(true);
         String msg = "Ciao sono un messaggio di broadcast";
         byte[] buf = new byte[1000];
         buf = msg.getBytes();
